f5c612762400cf4fc5e88ab3f787904b991afcea
Jesse <jessexinyu@foxmail.com>
2022-11-10 11:47:14 +0800

    update

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -1,9 +1,26 @@
 import { MidwayConfig } from '@midwayjs/core'
+import { Photo } from '@/entity/photo'
 
 export default {
   // use for cookie sign key, should change to your own and keep security
   keys: '1667972012973_8495',
   koa: {
     port: 7001,
+    globalPrefix: '/api'
   },
+  typeorm: {
+    dataSource: {
+      default: {
+        type: 'mysql',
+        host: '192.168.0.141',
+        port: 3307,
+        username: 'root',
+        password: '123456',
+        database: 'hk',
+        synchronize: true,
+        logging: false,
+        entities: [Photo]
+      }
+    }
+  }
 } as MidwayConfig
--- src/configuration.ts
+++ src/configuration.ts
@@ -2,6 +2,7 @@ import { Configuration, App } from '@midwayjs/decorator'
 import * as koa from '@midwayjs/koa'
 import * as validate from '@midwayjs/validate'
 import * as info from '@midwayjs/info'
+import * as orm from '@midwayjs/typeorm'
 import { join } from 'path'
 // import { DefaultErrorFilter } from './filter/default.filter';
 // import { NotFoundFilter } from './filter/notfound.filter';
@@ -14,10 +15,11 @@ import { ReportMiddleware } from './middleware/report.middleware'
     validate,
     {
       component: info,
-      enabledEnvironment: ['local'],
+      enabledEnvironment: ['local']
     },
+    orm
   ],
-  importConfigs: [join(__dirname, './config')],
+  importConfigs: [join(__dirname, './config')]
 })
 export class ContainerLifeCycle {
   @App()
